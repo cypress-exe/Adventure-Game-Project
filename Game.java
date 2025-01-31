f4d94c20d51b0993bb3c7fd3d92f0a6c33c31333
Dirk Thayer <dbt5b1fd@lcstudent.org>
2025-01-31 10:20:15 -0700

    Planning

--- Game.java
+++ Game.java
@@ -1,3 +1,18 @@
+/* 
+Game Path:
+Stuck in a labyrinth with pathways to choose from.
+
+The labyrinth's map will be regenerated every run.
+
+You get to choose what paths to take.
+
+There is an exit that you see fairly early on. However, you
+need to get a tool to break down the door.
+
+But just when you find a tool, you can go back and use it.
+
+*/
+
 public class Game {
     public static void main(String[] args) {
         
